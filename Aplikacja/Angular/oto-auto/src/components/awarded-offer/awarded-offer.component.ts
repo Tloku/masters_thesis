d636dd191ad1a8cd059b465c4b47dbfaa4d274cb
tloku <tlokinski.dominik@gmail.com>
2023-08-17 23:23:44 +0200

    Added awarded-offer card component and styled it

--- Aplikacja/Angular/oto-auto/src/components/awarded-offer/awarded-offer.component.ts
+++ Aplikacja/Angular/oto-auto/src/components/awarded-offer/awarded-offer.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { OfferCardData } from 'src/models/offer-card-data/offer-card-data..model';
 
 @Component({
   selector: 'awarded-offer',
   templateUrl: './awarded-offer.component.html',
   styleUrls: ['./awarded-offer.component.css'],
 })
-export class AwardedOfferComponent {}
+export class AwardedOfferComponent {
+  offer: OfferCardData = {
+    name: 'Mercedes-Benz CLA 180 d AMG Line 7G-DCT',
+    year: '2019',
+    mileage: '55039 km',
+    engineCapacity: '1 461 cm3',
+    fuelType: 'Diesel',
+    price: '143 500 PLN',
+  };
+}
