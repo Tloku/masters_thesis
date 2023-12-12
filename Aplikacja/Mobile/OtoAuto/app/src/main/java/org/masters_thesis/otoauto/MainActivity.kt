b0e08697ed05b214b9d12c9b6fa322e91ea9a715
tloku <tlokinski.dominik@gmail.com>
2023-12-12 23:00:13 +0100

    Added navigation to main activity

--- Aplikacja/Mobile/OtoAuto/app/src/main/java/org/masters_thesis/otoauto/MainActivity.kt
+++ Aplikacja/Mobile/OtoAuto/app/src/main/java/org/masters_thesis/otoauto/MainActivity.kt
@@ -1,9 +1,17 @@
 package org.masters_thesis.otoauto
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 import org.masters_thesis.otoauto.components.offerCardComponent.OfferCardComponentAdapter
 import org.masters_thesis.otoauto.components.offerCardComponent.OfferCardComponentService
 import org.masters_thesis.otoauto.logic.offer.OfferService
@@ -12,15 +20,34 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private val offerCardComponentService: OfferCardComponentService = OfferCardComponentService()
     private val offerCardService: OfferService = OfferService()
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initDrawer()
         initOfferAdapter()
     }
+
+    private fun initDrawer() {
+        drawerLayout = findViewById<DrawerLayout>(R.id.main_drawer)
+        navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.setHomeAsUpIndicator(R.drawable.menu)
+        navigationView.bringToFront()
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
 
     private fun initOfferAdapter() {
         val recyclerView = findViewById<RecyclerView>(R.id.offerCardRecyclerView)
@@ -49,5 +76,19 @@ class MainActivity : AppCompatActivity() {
                 println(t.printStackTrace());
             }
         })
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+            return
+        }
+
+        super.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+        return true;
     }
 }
