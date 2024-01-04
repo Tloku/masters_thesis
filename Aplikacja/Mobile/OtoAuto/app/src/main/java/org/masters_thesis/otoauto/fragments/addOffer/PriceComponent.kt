d93196567b23a3e52ada444c7cb7e44842315f00
Tloku <tlokinski.dominik@gmail.com>
2024-01-04 23:51:42 +0100

    Added skeletons of fragments

---
+++ Aplikacja/Mobile/OtoAuto/app/src/main/java/org/masters_thesis/otoauto/fragments/addOffer/PriceComponent.kt
@@ -0,0 +1,8 @@
+package org.masters_thesis.otoauto.fragments.addOffer
+
+import androidx.fragment.app.Fragment
+import org.masters_thesis.otoauto.R
+
+class PriceComponent : Fragment(R.layout.fragment_price_component) {
+
+}
---
+++ Aplikacja/Mobile/OtoAuto/app/src/main/java/org/masters_thesis/otoauto/fragments/addOffer/TechnicalDataComponent.kt
@@ -0,0 +1,12 @@
+package org.masters_thesis.otoauto.fragments.addOffer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.masters_thesis.otoauto.R
+
+class TechnicalDataComponent : Fragment(R.layout.fragment_technical_data_component) {
+
+}
