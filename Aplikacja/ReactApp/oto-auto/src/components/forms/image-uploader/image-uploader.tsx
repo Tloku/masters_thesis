0d3f1425c4257cdfce766ba4c9427f8c7feb4702
tloku <tlokinski.dominik@gmail.com>
2024-02-20 20:54:44 +0100

    Created drag and drop component

--- Aplikacja/ReactApp/oto-auto/src/components/forms/image-uploader/image-uploader.tsx
+++ Aplikacja/ReactApp/oto-auto/src/components/forms/image-uploader/image-uploader.tsx
@@ -1,18 +1,53 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState } from 'react';
+import { RefObject, useEffect, useRef, useState } from 'react';
 import './image-uploader.scss'
 
 
 
 export const ImageUploader: React.FC = () => {
     const [offerImagesData, setOfferImagesData] = useState<string[]>([])
+    const dropZone: RefObject<HTMLDivElement> = useRef(null);
 
 
-    return <div className='image-uploader-wrapper'>
+    useEffect(() => {
+        if (dropZone && dropZone.current) {
+            dropZone.current.addEventListener("dragover", handleDragOver)
+            dropZone.current.addEventListener("drop", onImageDrop)
+        }
+        
+        return () => {
+            dropZone!.current!.removeEventListener("dragover", handleDragOver)
+            dropZone!.current!.removeEventListener("drop", onImageDrop)
+        }
+    }, [])
+
+    const onImageDrop = (event: DragEvent) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const {files} = event.dataTransfer!;
+        console.log(files)
+    }
+    
+
+    return <div className='image-uploader-wrapper' ref={dropZone}>
         {
         !offerImagesData.length && 
         <div className="image-drop-zone">
+            <div className="button">
+                <input
+                    type="file"
+                    id="fileDropRef"
+                    multiple 
+                />
+                <label htmlFor="fileDropRef">Dodaj zdjęcia</label>
 
+                <h4>lub upuść pliki tutaj</h4>
+            </div>
+        
+            <div className="info-label">
+                <h6>Dodaj do 40 zdjęć w formatach jpg, png lub gif</h6>
+            </div> 
         </div>
         }
         
@@ -33,3 +68,8 @@ function determineMimeTypeIsImage(file: File): boolean {
     }
     return true;
 }
+
+const handleDragOver = (event: DragEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
